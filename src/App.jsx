a2e17a82c16228165abad25ce716e582f708ae0c
rCotche <roshan.cotche@gmail.com>
2025-08-18 10:08:56 +0400

    1:18:32

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Search from './components/search'
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
   //searchTerm : nom de la variable, le state, ce que je vais regarder
@@ -7,18 +19,69 @@ const App = () => {
   //bonne pratique : nomVariable, setNomVariable
   //useState('') : '' est la valeur par default de hasLiked (initialState)
   const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      //fonction fetch : get the data from apis
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error('reseponse : Failed to fetch movies');
+      }
+
+      const data = await response.json();
+      if (data.Response == 'False') {
+        setErrorMessage(data.Error || 'data response : Failed to fetch movies');
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+    } catch (error) {
+      console.log(`Error fetching movies (console): ${error}`)
+      setErrorMessage('try catch block : Error fetching movies, try later')
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  //sera utilise qu'une seul fois au chargement car
+  //on a mis une dependence vide
+  useEffect(() => {
+    fetchMovies();
+  }, [])
 
   return (
     <main>
-      <div className='pattern'/>
+      <div className='pattern' />
       <div className='wrapper'>
         <header>
-          <img src="./hero.png" alt="HeroBackground"/>
+          <img src="./hero.png" alt="HeroBackground" />
           <h1>Touvez les <span className='text-gradient'>films</span> rapidement</h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-        <h1>{searchTerm}</h1>
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          {isLoading ? (<p className="text-white">
+            Loading...
+          </p>) : errorMessage ? (<p className="text-red-500">
+            {errorMessage}
+          </p>) : (<ul>
+            {movieList.map((movie) => (
+              <p className="text-white">{movie.title}</p>
+            ))}
+          </ul>)}
+        </section>
       </div>
     </main>
   )
